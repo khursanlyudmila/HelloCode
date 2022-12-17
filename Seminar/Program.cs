7c72f4a03b75a9cef46b44f900f74327e0f3d65c
Khursan Lyudmila <khursanlyudmila@gmail.com>
2022-12-17 12:40:57 +0300

    add Example 007 Magic

---
+++ Seminar/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число:");
+
+string numberStr = Console.ReadLine() ?? "";// fsdfdsdsf, 1223423 "fsdfdsdsf", "1223423"
+int number = int.Parse(numberStr);// "323" -> 323
+if (number >=100 && number < 1000)
+{
+Console.WriteLine(number%10);
+}
+else
+Console.WriteLine("Число не соответствует заданным параметрам");
